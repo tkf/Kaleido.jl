ee454004eea1828225f309aea64e949c9286d2e0
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-26 19:23:46 -0700

    More documentation for batch and batchlens

--- src/batching.jl
+++ src/batching.jl
@@ -4,6 +4,33 @@
 From ``n`` lenses, create a single lens that gets/sets ``n``-tuple in
 such a way that the number of call to the constructor is minimized.
 This is done by calling [`IndexBatchLens`](@ref) whenever possible.
+
+# Examples
+```jldoctest
+julia> using Kaleido, Setfield
+
+julia> lens = @batchlens begin
+           _.a.b.c
+           _.a.b.d
+           _.a.e
+       end;
+
+julia> @assert lens ==
+           IndexBatchLens(:a) ∘ MultiLens((
+               (@lens _[1]) ∘ IndexBatchLens(:b, :e) ∘ MultiLens((
+                   (@lens _[1]) ∘ IndexBatchLens(:c, :d),
+                   (@lens _[2]) ∘ Kaleido.SingletonLens(),
+               )) ∘ FlatLens(2, 1),
+           )) ∘ FlatLens(3)
+
+julia> obj = (a=(b=(c=1, d=2), e=3),);
+
+julia> get(obj, lens)
+(1, 2, 3)
+
+julia> set(obj, lens, (10, 20, 30))
+(a = (b = (c = 10, d = 20), e = 30),)
+```
 """
 batch
 
@@ -53,23 +80,31 @@ _batch(lenses::Tuple{Vararg{Lens}}) = batch(lenses...)
         lens_expression_n
     end
 
+From ``n`` "lens expression", create a lens that gets/sets ``n``-tuple.
+Each "lens expression" is an expression that is supported by
+`Setfield.@lens` or such expression post-composed with other lenses
+using `∘`.
+
+See also [`batch`](@ref) which does all the heavy lifting of the
+transformation of the lenses.
+
 # Examples
 ```jldoctest
 julia> using Kaleido, Setfield
 
 julia> lens = @batchlens begin
            _.a.b.c
-           _.a.b.d
+           _.a.b.d ∘ BijectionLens(x -> parse(Int, x), string)
            _.a.e
        end;
 
-julia> obj = (a = (b = (c = 1, d = 2), e = 3),);
+julia> obj = (a = (b = (c = 1, d = "2"), e = 3),);
 
 julia> get(obj, lens)
 (1, 2, 3)
 
 julia> set(obj, lens, (10, 20, 30))
-(a = (b = (c = 10, d = 20), e = 30),)
+(a = (b = (c = 10, d = "20"), e = 30),)
 ```
 """
 macro batchlens(lenses_expression)
