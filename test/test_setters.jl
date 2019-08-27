b559f7032ab124338cd225e2665f518c5b9ab236
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-27 02:04:09 -0700

    Add test_setters.jl

---
+++ test/test_setters.jl
@@ -0,0 +1,14 @@
+module TestSetters
+
+include("preamble.jl")
+
+@testset "nullsetter" begin
+    @test set(1, nullsetter, 2) === 1
+end
+
+@testset "ToField" begin
+    setter = (@lens _.x) ∘ ToField(@lens _.a)
+    @test set((x = 1, y = 2), setter, (a = 10, b = 20)) === (x = 10, y = 2)
+end
+
+end  # module
